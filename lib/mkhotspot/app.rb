ae595907defc97483d8308be35596a5513ea8bb3
Pavel Argentov <argentoff@gmail.com>
2011-09-21 23:24:24 +0400

    Ground-up App rewriting
    Goal: use Thor::Group as a task grouping mechanism

--- lib/mkhotspot/app.rb
+++ lib/mkhotspot/app.rb
@@ -1,13 +1,10 @@
 require 'thor'
+require 'thor/group'
 require 'mkhotspot/version'
 
 module Mkhotspot
   class App < Thor
     include Thor::Actions
-
-    attr_reader :cfg
-
-    default_task :generate
 
     map %w(-v -V --version) => :version
 
@@ -17,29 +14,15 @@ module Mkhotspot
 
     desc "version", "Get the current version number"
     def version
-      say Mkhotspot::VERSION
+      say "Simple ISP hotspot generator version #{Mkhotspot::VERSION}"
     end
 
-    desc "generate HS_NAME", "Generate the configuration set for a hotspot"
-    method_option :cfile, :type => :string,  :default => nil,   :aliases => "-cf", :desc => "path to configuration file"
-    method_option :clean, :type => :boolean, :default => false, :aliases => "-c",  :desc => "Remove the hotspot dir before doing the generation job"
-    def generate(hs_name)
-      puts "Generating the configuration set for a hotspot #{hs_name}"
-    end
-
-    # Supplementary methods
-    no_tasks do
-
-      # Invokes the configuration parser
-      def configure(cfg_file)
-        if File.exists?(cfg_file)
-          @cfg ||= Mkhotspot::Config.new :file => cfg_file
-        else
-          say "Configuration file '#{cfg_file}' was not found!", :red
-        end
+    class Generator < Thor::Group
+      def configure
       end
 
+      def generate
+      end
     end
-
   end
 end
