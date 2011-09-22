8ae569ed9e1a0b2b4f6109016cd4797e3b36d4d9
Pavel Argentov <argentoff@gmail.com>
2011-09-22 11:00:42 +0400

    Found how to register a subtask

--- lib/mkhotspot/app.rb
+++ lib/mkhotspot/app.rb
@@ -8,21 +8,39 @@ module Mkhotspot
 
     map %w(-v -V --version) => :version
 
-    def self.source_root
-      File.expand_path('../../templates', File.dirname(__FILE__))
-    end
-
     desc "version", "Get the current version number"
     def version
       say "Simple ISP hotspot generator version #{Mkhotspot::VERSION}"
     end
 
     class Generator < Thor::Group
+      include Thor::Actions
+
+      class_option :f,
+          :type => :string,
+          :default => "hotspot.cfg"
+
+      desc %(see "help generate")
+
+      def self.source_root
+        File.expand_path('../../templates', File.dirname(__FILE__))
+      end
+
       def configure
+        say "foo", :green
       end
 
       def generate
+        say "bar", :green
       end
     end
+
+    register Generator,
+      :generate,
+      "generate",
+      "Generates the set of files defining an ISG hotspot"
+
+    default_task :generate
+
   end
 end
